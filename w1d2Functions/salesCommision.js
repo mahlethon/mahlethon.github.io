fbd3e56bbc6812ab926afc0db8a1e06de4a461f2
Mahlet Honelign <mhonelign@miu.edu>
2021-08-31 16:12:27 -0500

    Create salesCommision.js

---
+++ w1d2Functions/salesCommision.js
@@ -0,0 +1,28 @@
+function computeSalesCommision(isSalaried, salesAmount){
+    let commission;
+    if (isSalaried === true ){
+        if(salesAmount >= 300 && salesAmount <= 500){
+            commision = salesAmount*0.01;
+
+        }if(salesAmount > 500){
+            commission = salesAmount*0.02;
+
+        }else{
+            commission = 0;
+        }
+        
+    }else if(salesAmount >= 300 && salesAmount <= 500){
+        commision = salesAmount*0.02;
+
+    }if(salesAmount > 500){
+        commission = salesAmount*0.03;
+
+    }
+    return commission;
+}
+console.log( computeSalesCommission(true, 200));
+console.log( computeSalesCommission(false, 200));
+console.log(computeSalesCommission(true, 300));
+console.log( computeSalesCommission(false, 300));
+console.log( computeSalesCommission(true, 3500));
+console.log( computeSalesCommission(false, 3500));
