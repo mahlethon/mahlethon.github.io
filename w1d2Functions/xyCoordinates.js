8931e505e0f2fb647d726578a6d2c29e25ee030a
Mahlet Honelign <mhonelign@miu.edu>
2021-08-31 20:45:59 -0500

    Update xyCoordinates.js

--- w1d2Functions/xyCoordinates.js
+++ w1d2Functions/xyCoordinates.js
@@ -1,9 +1,9 @@
-function calculateDistance(x1,y1,x2,y2){
+function calcDistance(x1,y1,x2,y2){
     let distance;
 let xdifferenceSquare = Math.pow((x2-x1),2);
 let yDifferenceSquare = Math.pow((y2-y1),2);
 let sum = xdifferenceSquare+yDifferenceSquare;
-distance = Math.pow(sum,2);
+distance = Math.sqrt(sum,2);
 return distance;
 
 }
