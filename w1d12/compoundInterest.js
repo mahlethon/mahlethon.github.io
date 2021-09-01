7250b5b98e11e1451bfe46de587edcae7ad20ee2
mahlethon <89826475+mahlethon@users.noreply.github.com>
2021-08-31 20:42:39 -0500

    Delete w1d12 directory

--- w1d12/compoundInterest.js
+++
@@ -1,6 +0,0 @@
-function compoundInterest(initialAmount, annualInterestRate, years) {
-    let interst;
-     interest = initialAmount * (1 + annualInterestRate / 100 / 12) ** (years * 12) -
-      initialAmount;
-    return interst + initialAmount;
-  }
