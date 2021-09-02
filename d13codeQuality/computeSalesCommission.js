90b6ff0e029855d5c7269dd8613b85a324ed8819
Mahlet Honelign <mhonelign@miu.edu>
2021-09-01 21:20:29 -0500

    Merge branch 'main' of https://github.com/mahlethon/mahlethon.github.io into main

---
+++ d13codeQuality/computeSalesCommission.js
@@ -0,0 +1,52 @@
+function computeSalesCommission(isSalaried, salesAmount){
+    let commission = 0;
+    if (isSalaried === true ){
+        if(salesAmount >= 300 && salesAmount <= 500){
+            commission = salesAmount*0.01;
+
+        }if(salesAmount > 500){
+            commission = salesAmount*0.02;
+
+        }else{
+            commission = 0;
+        }
+        
+    }else {
+        if(salesAmount >= 300 && salesAmount <= 500){
+            commission = salesAmount*0.02;
+        }if(salesAmount > 500){
+            commission = salesAmount*0.03;
+       
+
+        }
+
+    }
+    return commission;
+}
+console.log( computeSalesCommission(true, 200));
+console.log( computeSalesCommission(false, 200));
+console.log(computeSalesCommission(true, 300));
+console.log( computeSalesCommission(false, 300));
+console.log( computeSalesCommission(true, 3500));
+console.log( computeSalesCommission(false, 3500));
+
+describe("test of ComputSalesCommission", function(){
+    it("tests salaried and 200 sales", function(){
+    assert.strictEqual(computeSalesCommission(true, 200), 0);
+    });
+    it("tests not salaried and 200 sales", function(){
+    assert.strictEqual(computeSalesCommission(false, 200), 0);
+    });
+    it("tests salaried and 300 sales", function(){
+    assert.strictEqual(computeSalesCommission(true, 300), 3);
+    });
+    it("tests not salaried and 300 sales", function(){
+    assert.strictEqual(computeSalesCommission(false, 300), 6);
+    });
+    it("tests salaried and 3500 sales", function(){
+    assert.strictEqual(computeSalesCommission(true, 3500), 65);
+    });
+    it("tests not salaried and 3500 sales", function(){
+    assert.strictEqual(computeSalesCommission(false, 3500), 100);
+    });
+   });
---
+++ d13codeQuality/d13mochaTests.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+describe("test of ComputSalesCommission", function(){
+    it("tests salaried and 200 sales", function(){
+    assert.strictEqual(computeSalesCommission(true, 200), 0);
+    });
+    it("tests not salaried and 200 sales", function(){
+    assert.strictEqual(computeSalesCommission(false, 200), 0);
+    });
+    it("tests salaried and 300 sales", function(){
+    assert.strictEqual(computeSalesCommission(true, 300), 3);
+    });
+    it("tests not salaried and 300 sales", function(){
+    assert.strictEqual(computeSalesCommission(false, 300), 6);
+    });
+    it("tests salaried and 3500 sales", function(){
+    assert.strictEqual(computeSalesCommission(true, 3500), 65);
+    });
+    it("tests not salaried and 3500 sales", function(){
+    assert.strictEqual(computeSalesCommission(false, 3500), 100);
+    });
+   });
