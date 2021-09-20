e0fe5ce1ed24aa19696691439ab29e42735bd4da
Mahlet Honelign <mhonelign@miu.edu>
2021-09-20 18:50:36 -0500

    d41

---
+++ d41inheritance/teacher.js
@@ -0,0 +1,44 @@
+"use strict";
+/* global exports */
+/* exports at end of file since exporting an object, which can only be referenced after definition 
+ comment out when running in browser */
+
+
+const Person = function() {};
+
+Person.prototype.initialize = function(name, age){
+    this.name = name;
+    this.age = age;
+};
+
+Person.prototype.describe = function(){
+    return this.name + ", " + this.age + " years old.";
+};
+
+const Student = function() {};
+Student.prototype = new Person();
+
+Student.prototype.learn = function(subject){
+    return (this.name + " just learned " + subject);
+};
+
+const john = new Student();
+
+john.initialize("John", 25);
+console.log(john.learn("Inheritance"));
+
+/* Create an object called Teacher derived from the Person class, and implement a method called teach which receives a string called subject, and prints out:
+
+[teacher's name] is now teaching [subject]
+*/
+const Teacher = function(){};
+Teacher.prototype = new Person();
+Teacher.prototype.teach = function(subject){
+return `${this.name} is now teaching ${subject}`;
+} 
+const bob = new Teacher();
+bob.initialize("Bob", 25);
+console.log(bob.teach("Physics"));
+
+/* comment out when running in browser */
+//module.exports = {Teacher, Student}; 
