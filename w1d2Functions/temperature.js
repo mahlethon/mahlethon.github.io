2f439b0a0ef9169e37b1ad1b09f0ebd779d30402
mahlethon <89826475+mahlethon@users.noreply.github.com>
2021-10-18 23:34:54 -0500

    Delete w1d2Functions directory

--- w1d2Functions/temperature.js
+++
@@ -1,8 +0,0 @@
-function convertFahrenheit(farenheitTemp){
-    let celsiusTemp = (farenheitTemp-32)*(5/9);
-    return celsiusTemp;
-    }
-    console.log( convertFahrenheit (32));
-    console.log( convertFahrenheit (0));
-    console.log( convertFahrenheit (212));
-    console.log( convertFahrenheit (100));
