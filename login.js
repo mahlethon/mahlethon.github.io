b243ba1c9e65f63d0d002fbd05b2136c4ab0e38e
Mahlet Honelign <mhonelign@miu.edu>
2021-10-20 13:31:58 -0500

    update

--- login.js
+++ login.js
@@ -6,6 +6,9 @@ window.onload= function(){
         if( document.getElementById("name").value == "name" &&  document.getElementById("password").value =="pwd"){
             document.getElementById("form").action = "./dashboard.html";
 
+        }else {
+            alert("Incorrect credentials! Please check your email and password.");
+             
         }
         
   
@@ -15,5 +18,4 @@ window.onload= function(){
 
 }
        
-    
     
